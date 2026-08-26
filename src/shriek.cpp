bc98cf84b79b27309bc56bc2bed7343efff7b2f5
tausif <tausif.core@gmail.com>
2026-08-26 20:16:37 +0530

    added data types for format

--- src/shriek.cpp
+++ src/shriek.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+struct Sub {
+  char id[4];
+  char *command;
+};
+
+struct Topic {
+  char name[32];
+  Sub *subs;
+};
+
 int main(int argc, char* argv[]) {
     std::cout << "Shriek!" << std::endl;
     return 0;

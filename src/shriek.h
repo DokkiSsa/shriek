db30f5ec518d049b882c5e4a973da32404eeed3f
tausif <tausif.core@gmail.com>
2026-08-26 20:16:38 +0530

    added dir functions

--- src/shriek.h
+++ src/shriek.h
@@ -1,9 +1,15 @@
 #ifndef SHRIEK_H
 #define SHRIEK_H
 
+#include <filesystem>
+#include <string>
+#include <vector>
+
 #include <ctype.h>
 
 #define TOPIC_MAX 32
+
+namespace fs = std::filesystem;
 
 struct Sub
 {
@@ -32,6 +38,27 @@ const bool isValidTopicName(const char *str)
 inline const bool isValidId(const int id)
 {
   return id > 0;
+}
+
+std::vector<std::string> getAllFilesInPath(const std::string &path)
+{
+  std::vector<std::string> files;
+  for (const auto &entry : fs::directory_iterator(path))
+    if (entry.is_regular_file())
+      files.push_back(entry.path().string());
+  return files;
+}
+
+std::string getOneFileInPath(const std::string &path, const std::string &fileName)
+{
+  for (const auto &entry : fs::directory_iterator(path))
+    if (entry.is_regular_file())
+    {
+      const auto &filePath = entry.path();
+      if (filePath.filename().string() == fileName)
+        return filePath.string();
+    }
+  return {}; // Return empty string if no file found
 }
 
 #endif // SHRIEK_H

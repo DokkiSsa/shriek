00178e0d6b7a9bd4e4870e4f0a878c33bc18bb6c
tausif <tausif.core@gmail.com>
2026-08-26 20:16:38 +0530

    added helper funcs

--- src/shriek.h
+++ src/shriek.h
@@ -1,20 +1,37 @@
 #ifndef SHRIEK_H
 #define SHRIEK_H
 
-#ifndef VERSION
-  #define VERSION "1.0.0"
-#endif
+#include <ctype.h>
 
-struct Sub {
-  int id; 
+#define TOPIC_MAX 32
+
+struct Sub
+{
+  int id;
   char *command;
 };
 
-struct Topic {
+struct Topic
+{
   // 31 characters for the topic name, plus a null terminator
-  char name[32];
+  char name[TOPIC_MAX];
   Sub *subs;
 };
 
+const bool isValidTopicName(const char *str)
+{
+  int length = 1;
+  if (!str || !*str || !isalpha(*str))
+    return false;
+  while (*++str)
+    if ((!isalnum(*str) && *str != '_') || length++ > TOPIC_MAX - 1)
+      return false;
+  return true;
+}
+
+inline const bool isValidId(const int id)
+{
+  return id > 0;
+}
 
 #endif // SHRIEK_H
